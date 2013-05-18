181ab2919c0b0211f6aa6fd33b7078cefcb2d6b2
Prak <www.prak.co.in@gmail.com>
2013-05-18 20:08:26 +0530

    Added for loop different method

--- Loops/for.sh
+++ Loops/for.sh
@@ -1,4 +1,6 @@
-#! /bin/sh
+#! /bin/bash
+
+echo "bash version $BASH_VERSION ..."
 
 
 echo "Method 1"
@@ -8,5 +10,33 @@ for i in 1 2 3 4 5 6 7 8 9 10
 do
 	echo $i
 done
+ 
+echo "==============================================="
 
+echo "Method 2"
+echo "Printing from value 1 to 5"
+for i in {1..5}
+do
+	echo $i
+done
 
+echo "==============================================="
+
+echo "Method 3"
+echo "Printing from value 0 to 20"
+for i in {1..20..2}
+do
+	echo $i
+done
+
+echo "==============================================="
+
+echo "Method 4"
+echo "Printing from value 1 to 10"
+Max=10
+for i in `seq 1 $Max`
+do
+	echo $i;
+done
+
+echo "==============================================="
